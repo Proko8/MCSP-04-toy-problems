7b426e45f0c0eda0eb090b775b2eb3f77bbdd524
Paul <pprokopy@gmail.com>
2020-04-30 09:49:40 -0500

    balancedBrackets

--- balancedBrackets/balancedBracets.js
+++ balancedBrackets/balancedBracets.js
@@ -9,4 +9,49 @@
 // str: "[{()}]"                  |   true
 // str: "(50)("                   |   false
 // str: "[{]}"                    |   false
-const isBalanced = str => {};
+const isBalanced = str => {
+let balanced = {
+    "parenthesisOpen" : 0,
+    "parenthesisClosed" : 0,
+    "curlyBracketsOpen" : 0, 
+    "curlyBracketsClosed" : 0,
+    "squareBracketsOpen" : 0,
+    "squareBracketsClosed" : 0,
+}
+    for(var i = 0; i < str.length; i++){
+        if(str[i] === "("){
+            balanced["parenthesisOpen"] += 1;
+        }
+        if(str[i] === ")"){
+            balanced["parenthesisClosed"] += 1;
+        }
+        if(str[i] === "{"){
+            balanced["curlyBracketsOpen"] += 1;
+        }
+        if(str[i] === "}"){
+            balanced["curlyBracketsClosed"] += 1;
+        }
+        if(str[i] === "["){
+            balanced["squareBracketsOpen"] += 1;
+        }
+        if(str[i] === "]"){
+            balanced["squareBracketsClosed"] += 1;
+        }
+    }
+    if(balanced["parenthesisOpen"] !== balanced["parenthesisClosed"]){
+        return false;
+    }
+    if(balanced["curlyBracketsOpen"] !== balanced["curlyBracketsClosed"]){
+        return false;
+    }
+    if(balanced["squareBracketsOpen"] !== balanced["squareBracketsClosed"]){
+        return false;
+    }
+    return true;
+};
+
+// create an object with a parenthesisOpen, parenthesisClosed, curlyBracketsOpen, curlyBracketsClosed, squareBracketsOpen, squareBracketsClosed keys
+// iterate through the string
+// create 6 if statements asking if [i] === (,),{,},[,] 
+// if so increment that key in the object
+// create 3 more conditionals seeing if the open/close numbers are equal, if so true, if not false
