e11aa6b4e2b8c2cd010a4dd236ff69974ff08c74
Paul <pprokopy@gmail.com>
2020-05-12 09:13:08 -0500

    bogglesolve

--- boggleSolve/boggleSolve.js
+++ boggleSolve/boggleSolve.js
@@ -3,4 +3,16 @@
 // * i.e. a=1, z=26 etc.
 // return the sum of the values of each letter in string
 
-const boggleSolve = str => {};
+const boggleSolve = str => {
+    let counter = 0;
+    for(var i = 0; i < str.length; i++){
+        counter += i;
+    }
+    return counter;
+};
+
+
+//create a counter variable
+//iterate over the string
+//counter += string[i]
+//return counter
